9d10e23518b2d93ab7fe2535d9c70c94fcdff8fd
Emily Reed <emilyreed89@gmail.com>
2022-08-27 13:06:50 -0400

    more string katas

--- stringWars.js
+++ stringWars.js
@@ -2,9 +2,7 @@
 
 //P str -word
 //R boolean
-
 //E puppy -> false more than one p
-
 //P So I need to check if str includes an isogram. It can not have a duplicate and this is case insensitive
 
 function isIsogram(str){
@@ -52,3 +50,80 @@ function mirror(text){
     let addMiddle = wordArr.map((item, idx) => idx !== wordArr.length - 1 ? item.padEnd(longestWord, ' ') + ' ' + '*\n*' : item.padEnd(longestWord, ' ') + ' ').join(' ')
     return firstStars + addMiddle + secondStars
   }
+
+
+//7kyu find the capitals
+//Meh too easy so I added a helper function to practice with those more
+
+
+//P string
+//R array of itegers at index 
+//E 'hELoO' -> [1, 2, 4]
+//P Need to check string and place any indexes that contain a capital letter into a new array in order.
+
+var capitals = function (word) {
+	//create array to place indexs
+  const capitalIndex = []
+  //will need to iterate through the word now and check each letter
+  for(let i = 0; i < word.length; i++){
+    //take letter and go to function checkLetter to test if its capital or not
+    if(checkLetter(word[i])){
+    //if so push the index
+      capitalIndex.push(i)
+    }
+  }
+  //return my array of indexes
+  return capitalIndex
+};
+
+function checkLetter(letter){
+  //if word[i] matches word[i] uppercased return true, else return false
+  return letter == letter.toUpperCase()
+}
+
+//6kyu Who Likes This?
+//I thought about loops but why add time complexity when its not needed? 
+
+// //P array of strings
+// //R string 
+// //E [peter] -> peter likes this
+// //P Create a new sentence depending on how many names in array. If one just likes this, if two Jacob and Alex, if three add comma, more than 3 by how many names left after using first 3
+
+function likes(names) {
+  if(names.length == 0) return 'no one likes this'
+  if(names.length == 1) return `${names} likes this`
+  if(names.length == 2) return `${names[0]} and ${names[1]} like this`
+  if(names.length == 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+}
+
+likes([])//, 'no one likes this');
+likes(['Peter'])//, 'Peter likes this');
+likes(['Jacob', 'Alex'])//, 'Jacob and Alex like this');
+likes(['Max', 'John', 'Mark'])//, 'Max, John and Mark like this');
+likes(['Alex', 'Jacob', 'Mark', 'Max'])//, 'Alex, Jacob and 2 others like this')
+
+
+//6kyu Your order, please
+//I really liked this one. I saw a solution awhile back using a helper function to sort and I thought this would work perfect here (and it did)
+
+
+//P string of words containing a num inside each word
+//R string sorted by nums inside
+//E "" -> ""
+//  "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+//P I need to sort each word by the number it contains in ascending order.
+//if I use a helper function to split the word itself find the number and just sort by the number
+
+function order(words){
+    //splitting into words here so I can use sort method 
+    //in sort run my helper function and sort the words by the number in findNum then join them back to a string plz
+    return words.split(' ').sort((a, b) => findNum(a) - findNum(b)).join(' ')
+  }
+  
+  function findNum(str){
+    //hey friend can you help and split my word into chars now and if not between '0' and '9' (comparison operators) keep looking? k thanks, oh when you find it stop
+    return str.split('').find(item => item >= '0' && item <= '9')
+  }
+   
+  
